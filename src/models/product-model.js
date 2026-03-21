1221b0839579c06c24bf73b448816480e00618f0
fillipegabriel1 <fillipegabrielkb4@gmail.com>
2026-03-21 17:41:18 -0300

    adição de produtos

--- src/models/product-model.js
+++ src/models/product-model.js
@@ -1,15 +1,9 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    price: { type: Number, required: true },
-    inStock: { type: Boolean, default: true },
-    quantity: { type: Number, default: 0, required: true },
-    validity: { type: Date, required: true },
-    manufacturingDate: { type: Date, required: true }
+  nome: String,
+  preco: Number,
+  quantidade: Number
 });
 
-const Product = mongoose.model('Product', productSchema);
-
-export default Product;
+export default mongoose.model("Product", productSchema);
